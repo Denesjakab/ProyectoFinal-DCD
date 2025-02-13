5d321f04d67a7677ec059a85baace05aeace5ccd
CeliaBP <Celia.bcn28@gmail.com>
2025-02-13 10:44:01 +0000

    get clients

--- src/front/js/pages/PerfilCliente.js
+++ src/front/js/pages/PerfilCliente.js
@@ -3,7 +3,11 @@ import "../../styles/PerfilCliente.css";
 import { Link } from 'react-router-dom';
 
 const PerfilCliente = () => {
+  
+
   return (
+
+    
     <div className="container perfil-cliente">
       <div className="parte-arriba">
 
