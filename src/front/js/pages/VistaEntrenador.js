58f323bfc34c5bab00eadf7dab18109f431f93ae
CeliaBP <Celia.bcn28@gmail.com>
2025-02-13 10:44:01 +0000

    get clients

--- src/front/js/pages/VistaEntrenador.js
+++ src/front/js/pages/VistaEntrenador.js
@@ -10,23 +10,25 @@ const VistaEntrenador = () => {
     useEffect(() => {
         const userToken = localStorage.getItem("token")
         actions.getClients(userToken)
-     },[])
+    }, [])
 
-    
+
     return (
         <div className='vista-entrenador'>
             <div className='your-clients'>
                 <h1>Your Clients</h1>
 
                 <div className='lista-clientes'>
-                    {store.clients.map((client,index) => {
+                    {store.clients.map((client, index) => {
                         return (
-                        <div className='cliente' 
-                        key={index}
-                        >
-                            <p><strong>{client.client.name}</strong></p>
-                            <button className='view-details'>View details</button>
-                        </div>
+                            <div className='cliente'
+                                key={index}
+                            >
+                                <p><strong>{client.client.name}</strong></p>
+            
+                                    <button className='view-details'>View details</button>
+                               
+                            </div>
                         )
                     })
                     }
