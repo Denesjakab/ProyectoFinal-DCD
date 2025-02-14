e0bc329554195477b6dca834efedb098b92bd340
Daniel Campón Perdigones <campondaniel@gmail.com>
2025-02-14 17:16:35 +0000

    Correcciones y scroll en VistaEnt

--- src/front/js/pages/VistaEntrenador.js
+++ src/front/js/pages/VistaEntrenador.js
@@ -1,32 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import '../../styles/VistaEntrenador.css'
 import { Link } from 'react-router-dom';
+import { Context } from "../store/appContext.js";
+
 
 const VistaEntrenador = () => {
+    const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        const userToken = localStorage.getItem("token")
+        actions.getClients(userToken)
+    }, [])
+
+
     return (
         <div className='vista-entrenador'>
             <div className='your-clients'>
                 <h1>Your Clients</h1>
 
                 <div className='lista-clientes'>
-                    <div className='cliente'>
-                        <p><strong>Client Name</strong></p>
-                        <button className='view-details'>View details</button>
-                    </div>
-                    <div className='cliente'>
-                        <p><strong>Client Name</strong></p>
-                        <button className='view-details'>View details</button>
-                    </div>
-                    <div className='cliente'>
-                        <p><strong>Client Name</strong></p>
-                        <button className='view-details'>View details</button>
-                    </div>
-                    <div className='cliente'>
-                        <p><strong>Client Name</strong></p>
-                        <Link to="/trainer/perfilcliente">
+                    {store.clients.map((client, index) => (
+                        <div className='cliente' key={index}>
+                            <p><strong>{client.client.name}</strong></p>
                             <button className='view-details'>View details</button>
-                        </Link>
-                    </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
