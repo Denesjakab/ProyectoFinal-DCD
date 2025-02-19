72ca79b578548329bb2b1eb0f7956bd6419c2998
CeliaBP <Celia.bcn28@gmail.com>
2025-02-19 15:38:38 +0000

    minor design changes

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import '../../styles/footer.css'
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-1 text-center fixed-bottom">
+	<footer className="footer mt-auto py-1 text-center">
 		<p>
 			Copyright 2025
 		</p>
